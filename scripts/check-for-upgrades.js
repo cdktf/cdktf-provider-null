d14f31d5a1284b452eef668dcd0e9735a9f49aae
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-11-03 00:25:43 +0000

    chore(deps): upgrade dependencies
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdktf/cdktf-provider-null/actions/runs/3382109030
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- scripts/check-for-upgrades.js
+++ scripts/check-for-upgrades.js
@@ -11,7 +11,7 @@ const actions = require("@actions/core");
 
 // set by the projen file that generates this script
 const CONSTRAINT = "~> 3.0";
-const FQ_PROVIDER_NAME = "null";
+const FQ_PROVIDER_NAME = "hashicorp/null";
 
 (async function main() {
   if (await newerMatchingProviderVersionExists()) {
