1f1995f3a0533ad5d6b3fc14b3cd24ff301f4479
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-02 11:59:09 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/null/3.2.2/docs
 // generated from terraform resource schema
 
